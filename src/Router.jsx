39be62ac8482f5d290f6e493ed7c5724417699e1
Luis Martins <lulufan.12@gmail.com>
2021-06-26 11:24:29 -0300

    Created some UI components for the project and also created the login page design.

---
+++ src/Router.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Switch from './Switch';
+
+const router = () => (
+  <BrowserRouter>
+    <Switch />
+  </BrowserRouter>
+);
+
+export default router;
---
+++ src/components/HourRegisterPage/HourRegisterPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const hourRegisterPage = () => (
+  <div>
+    <h1>Página do Registro de Horas</h1>
+  </div>
+);
+export default hourRegisterPage;
